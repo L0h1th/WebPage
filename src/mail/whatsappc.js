d871ae56fa61cbc5f84f6159a03f1e9de568e28e
Lohith <72035486+L0h1th@users.noreply.github.com>
2024-09-02 07:29:53 +0000

    Contact card added

---
+++ src/mail/whatsappc.js
@@ -0,0 +1,21 @@
+function sendToWhatsappc(){
+    let numberra = "+919110280326";
+    // let numbersa = "+917019051108";
+    let name = document.getElementById('namec').value; 
+    let email = document.getElementById('emailc').value;
+    let subject = document.getElementById('subjectc').value;
+    let message = document.getElementById('messagec').value;
+
+    var url = "http://wa.me/" + numberra + "?text="
+    + "Name : " +name+ "%0a"
+    + "Email/Number : " +email+ "%0a"
+    + "Subject: " +subject+ "%0a"
+    + "Message : " +message+ "%0a";
+
+    // var url = "http://wa.me/" + numbersa + "?text="
+    // + "Name : " +name+ "%0a"
+    // + "Email/Number : " +email+ "%0a";
+
+
+    window.open(url, '_blank').focus();
+}
