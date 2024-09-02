a1e34bf96cfcce37b63ae4f065af89525cbc97e1
Lohith <72035486+L0h1th@users.noreply.github.com>
2024-09-02 06:02:10 +0000

    About modified

--- src/mail/whatsapp.js
+++ src/mail/whatsapp.js
@@ -1,11 +1,17 @@
 function sendToWhatsapp(){
-    let number = "+919110280326";
+    let numberra = "+919110280326";
+    // let numbersa = "+917019051108";
     let name = document.getElementById('name').value; 
     let email = document.getElementById('email').value;
 
-    var url = "http://wa.me/" + number + "?text="
+    var url = "http://wa.me/" + numberra + "?text="
     + "Name : " +name+ "%0a"
     + "Email/Number : " +email+ "%0a";
+
+    // var url = "http://wa.me/" + numbersa + "?text="
+    // + "Name : " +name+ "%0a"
+    // + "Email/Number : " +email+ "%0a";
+
 
     window.open(url, '_blank').focus();
 }
